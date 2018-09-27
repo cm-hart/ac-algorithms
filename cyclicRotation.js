72265a3a3761abbccf28cec8f0278963485c3ed7
cmh <cmh@anniecannons.com>
2018-09-27 10:46:34 -0700

    cyclicrRotation exercise

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -14,6 +14,26 @@
 //
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
 
-function cyclicRotation(arr, rotation){
-    //Your code here...
+//input: an array & a number of times rotated
+//output: an array after rotation
+//purpose: to rotate the items in an array 
+
+//pop will grab the the last thing off the end of an array
+//.unshift will add it to the beginning
+//Use a for loopto loop through the array for rotation number of times.
+//we need to pop the last element of the array off
+//Save the output of .pop to a variable
+//unshift (add to the beginning) the variable to the beginning of the array
+//return the array
+function cyclicRotation(arr, rotation) {
+    for (var i = 0; i< rotation; i++){
+        var drop = arr.pop();
+        arr.unshift(drop);
+    }
+    return arr;
 }
+
+var result = cyclicRotation([3, 8, 9, 7, 6], 1);
+console.log(result);
+//variable - will be an empty array
+
