5d942bbf65998e411ed3a7663a3730877b068042
cmh <cmh@anniecannons.com>
2018-10-01 10:52:13 -0700

    Completed factorializeIt exercise

--- factorializeIt.js
+++ factorializeIt.js
@@ -7,8 +7,24 @@
 
 // Check out Arithmetic Operators https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+
+//input: a number
+//output: a number; the factorial of the input number
+//purpose: so that you can come up with different combinations of things; 
+//create a new variable, that will store the output
+//create a for loop, the loop will run from 1 to the value of the inout number
+//multiply the value of our new variable by the current value of i (aka our counter)
+//return 
 function factorializeIt(num) {
-  //your code here
+  var factorialized = 1; 
+  for(var i =1; i<= num; i++){
+    factorialized *=i;
+  }
+  return factorialized;
 }
 
-factorializeIt(5);
+
+var result = factorializeIt(4); 
+console.log(result);
+
+
