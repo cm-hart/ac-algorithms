fcddebfdd718942f74ac616f92b23579857cdcca
cmh <cmh@anniecannons.com>
2018-10-02 11:05:55 -0700

    completed longest word algorithm

--- longestWord.js
+++ longestWord.js
@@ -18,8 +18,21 @@
 // findLongestWord("What is the average airspeed velocity of an unladen swallow") should return 8.
 // findLongestWord("What if we try a super-long word such as otorhinolaryngology") should return 19.
 
+//input: string of words
+//output: a number, letting us know the number of characters in the longest word in the string
+//purpose: to be able to count characters of single words within a multi-word string
+
 
 function findLongestWord(str) {
-  //your code here
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
 }
 
+var result = findLongestWord("The quick brown fox jumped over the lazy dog");
+console.log(result);
