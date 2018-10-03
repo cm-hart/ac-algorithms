22a5e1e2c63b82566ccba3bd0f31e28d73b58807
cmh <cmh@anniecannons.com>
2018-10-03 11:00:37 -0700

    dashInsert completed exercise

--- dashInsert.js
+++ dashInsert.js
@@ -25,6 +25,27 @@ console.log(result) // "1-3"
 
 **/
 
+//input: a string of numbers that contains at least one digit
+//output: a string, also with numbers but with dashes this time, between any two odd numbers
+//purpose: to insert dashes at specific places in a string
+
+//Split the string into an array of numbers
+//Store the array in a variable
+//Create a variab;e to store the output of the loop
+//Create a for loop that loops through the numbers (aka, the elements in the array)
+//	//Conditional statement that sees if the number we are 			looking at and the number that comes next in te array are both odd
+		//if they are, we want to add the number itself and a dash to the variable storing the output
+//Return the string, aka the variable storing the output of the string
+
 function dashInsert(str) {
-	// Your code
+	var arrayNumbers = str.split("")
+	var newString = "";
+	for (var i = 0; i < arrayNumbers.length; i++){
+		if(arrayNumber[i] % 2 === 1 && arrayNumber[i + 1] % 2 === 1){
+			newString = newString + arrayNumbers[i] + "-";
+		} else {
+			newSrting = newString + arrayNumbers[i];
+		}
+	}
+	return newString;
 }
